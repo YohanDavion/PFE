9f7659a0b9d5661f0b4a823ec21895e591c5b8da
valentin <valentin.smn81@gmail.com>
2025-01-10 15:15:37 +0100

    Initialisation Back

---
+++ src/test/java/fr/limayrac/pfeback/PfeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.limayrac.pfeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
