2fe6afbd43938ddb3e79023672333a53638500be
YohanDavion <yohandavion31@gmail.com>
2025-01-10 16:55:37 +0100

    init(front): Initialisation du projet Angular

---
+++ pfe-front/src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })]
+};
