406324ab58da6cb332bf7188972fc9ff6d097388
valentin <valentin.smn81@gmail.com>
2025-01-10 18:09:39 +0100

    fix(back) : Ajout du back à la bonne localisation

---
+++ pfe-back/src/main/java/fr/limayrac/pfeback/PfeBackApplication.java
@@ -0,0 +1,13 @@
+package fr.limayrac.pfeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfeBackApplication.class, args);
+	}
+
+}
